69e5f6a5f789c4f50a32dfe5e23582bf4ba8e527
MatiCBL <brzoza.mathew@gmail.com>
2023-06-08 19:21:18 +0100

    'create-community-modal-added'

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,25 @@
 import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
-import SearchInput from "./SearchInput";
-import RightContent from "./RightContent.tsx/RightContent";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/clientApp";
+import Directory from "./Directory/Directory";
+import RightContent from "./RightContent.tsx/RightContent";
+import SearchInput from "./SearchInput";
 
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
   return (
-    <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex align="center">
+    <Flex
+      bg="white"
+      height="44px"
+      padding="6px 12px"
+      justify={{ md: "space-between" }}
+    >
+      <Flex
+        align="center"
+        width={{ base: "40px", md: "auto" }}
+        mr={{ base: 0, md: 2 }}
+      >
         <Image src="/images/redditFace.svg" height="30px" alt="reddit logo" />
         <Image
           src="images/redditText.svg"
@@ -18,8 +28,8 @@ const Navbar: React.FC = () => {
           alt="reddit logo text"
         />
       </Flex>
-      {/* <Directory /> */}
-      <SearchInput />
+      {user && <Directory />}
+      <SearchInput user={user} />
       <RightContent user={user} />
     </Flex>
   );
